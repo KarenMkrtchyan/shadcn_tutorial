ba8b3ddc4a1bdbf02f1dfeef105f96c5a93d62ae
KarenMkrtchyan <70455623+KarenMkrtchyan@users.noreply.github.com>
2025-10-24 17:26:44 -0700

    cards with recipes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,63 @@
-export default function Home() {
-  return <main>home</main>;
+import {
+  Card,
+  CardHeader,
+  CardFooter,
+  CardTitle,
+  CardAction,
+  CardDescription,
+  CardContent,
+} from "@/components/ui/card";
+import { Badge } from "@/components/ui/badge";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+
+interface Recipe {
+  title: string;
+  image: string;
+  time: number;
+  description: string;
+  vegan: boolean;
+  id: string;
+}
+
+async function getRecipes(): Promise<Recipe[]> {
+  const result = await fetch("http://localhost:4000/recipes", {
+    // This ensures consistent data between server and client
+    cache: "force-cache",
+    // Optional: you can add revalidation if you want to refresh periodically
+    // next: { revalidate: 3600 } // revalidate every hour
+  });
+
+  return result.json();
+}
+
+export default async function Home() {
+  const recipes = await getRecipes();
+  return (
+    <main>
+      <div className="grid grid-cols-3 gap-8">
+        {recipes.map((recipe) => (
+          <Card key={recipe.id} className="flex flex-col justify-between">
+            <CardHeader className="flex flex-row gap-4 items-center">
+              <Avatar>
+                <AvatarImage src={`/img/${recipe.image}`} alt="recipe image" />
+                <AvatarFallback>{recipe.title.slice(0, 2)}</AvatarFallback>
+              </Avatar>
+              <div>
+                <CardTitle>{recipe.title}</CardTitle>
+                <CardDescription>{recipe.time} mins to cook</CardDescription>
+              </div>
+            </CardHeader>
+            <CardContent>
+              <p>{recipe.description}</p>
+            </CardContent>
+            <CardFooter className="flex justify-between">
+              <Button>View Recipe</Button>
+              {recipe.vegan && <Badge variant="secondary">Vegan!</Badge>}
+            </CardFooter>
+          </Card>
+        ))}
+      </div>
+    </main>
+  );
 }
